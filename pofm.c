1d409618b083e67821392712201c83b4d210211b
Aidan Johnston <aidanjhnston@gmail.com>
2019-10-31 09:33:41 -0400

    Tested args

--- pofm.c
+++ pofm.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int agrc, char* args[]) {
+    printf("%d", agrc);
+    for(int i = 0; i < agrc; i ++) {
+        printf("%s\n", args[i]);
+    }
     return 0;
 }
