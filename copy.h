1b911f366370d30ac0d9f0404688d7a0c1b71361
Aidan Johnston <aidanjhnston@gmail.com>
2019-11-18 17:09:41 -0500

    Added intentional logic

--- copy.h
+++ copy.h
@@ -45,5 +45,110 @@ bool doesFileExist(char* filename) {
 }
 
 bool copy(ARGS arguments) {
-    printf("%s\n", arguments.input_file);
+
+    char* option = "";
+    bool flag = false;
+    if(doesFileExist(arguments.input_file)) {
+        if(arguments.force) {
+            FILE* input = fopen(arguments.input_file, "r");
+            if(input == NULL)
+                printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+            FILE* output =  fopen(arguments.output_file, "w");
+            if(output == NULL)
+                printf("Couldn't open %s\n", arguments.output_file), exit(1);
+
+            char ch = "";
+            while ((ch = fgetc(input)) != EOF)
+                fputc(ch, output);
+
+            fclose(input);
+            fclose(output);
+
+            if(arguments.verbose)
+                printf("Copied files\n");
+        }
+        else {
+            if(arguments.output) {
+                if(doesFileExist(arguments.output_file)) {
+                    printf("Are you sure you want to copy %s?  %s already exist. (y/n)\n", arguments.input_file, arguments.output_file);
+                    scanf("%c", &option);
+
+                    while(!flag) {
+                        if(option == 121 || option == 110) {
+                            flag = true;
+                        }
+                        else {
+                            scanf("%c", &option);
+                            printf("Unknown answer, please try again: (y/n)\n");
+                            scanf("%c", &option);
+                        }
+                    }
+                    if(option == 121) {
+                        FILE* input = fopen(arguments.input_file, "r");
+                        if(input == NULL)
+                            printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+                        FILE* output = fopen(arguments.output_file, "w");
+                        if(output == NULL)
+                            printf("Couldn't open file\n"), exit(1);
+
+                        char ch = "";
+                        while((ch = fgetc(input)) != EOF)
+                            fputc(ch, output);
+
+                        fclose(input);
+                        fclose(output);
+
+                        if(arguments.verbose)
+                            printf("Copied the files\n");
+                    }
+                    else if(arguments.verbose) {
+                        printf("Wont copy the files\n");
+                    }
+                }
+                else {
+                    FILE* input = fopen(arguments.input_file, "r");
+                    if(input == NULL)
+                        printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+                    FILE* output = fopen(arguments.output_file, "w");
+                    if(output == NULL)
+                        printf("Couldn't open file\n"), exit(1);
+
+                    char ch = "";
+                    while((ch = fgetc(input)) != EOF)
+                        fputc(ch, output);
+
+                    fclose(input);
+                    fclose(output);
+
+                    if(arguments.verbose)
+                        printf("Copied the files\n");
+                }
+            }
+            else {
+                FILE *input = fopen(arguments.input_file, "r");
+                if (input == NULL)
+                    printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+                FILE *output = fopen("default.txt", "w");
+                if (output == NULL)
+                    printf("Couldn't open file\n"), exit(1);
+
+                char ch = "";
+                while ((ch = fgetc(input)) != EOF)
+                    fputc(ch, output);
+
+                fclose(input);
+                fclose(output);
+
+                if (arguments.verbose)
+                    printf("Copied the files\n");
+            }
+        }
+    }
+    else if(arguments.verbose) {
+        printf("The file %s doesn't exist, could not copy.\n");
+    }
 }
