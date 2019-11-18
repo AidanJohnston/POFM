5b7585ed47d3cd1546451d835ccb1b8fccff52bb
Aidan Johnston <aidanjhnston@gmail.com>
2019-11-18 15:42:15 -0500

    Added logic to remove.h

--- remove.h
+++ remove.h
@@ -20,5 +20,39 @@
 #include "header.h"
 
 bool removeFile(ARGS arguments) {
-    return true;
+
+    bool flag = false;
+    char option;
+
+    if(doesFileExist(arguments.input_file)) {
+        if (arguments.force) {
+            remove(arguments.input_file);
+            if (arguments.verbose)
+                printf("The file %s was removed.\n", arguments.input_file);
+        }
+        else {
+            printf("Are you sure you want to remove %s\n", arguments.input_file);
+            scanf("%c", &option);
+
+            while(!flag) {
+                if(option == "y" || option == "Y" || option == "n" || option == "N") {
+                    flag = true;
+                }
+                else {
+                    printf("Unknown answer, please try again: \n");
+                    scanf("%c", &option);
+                }
+            }
+            if(option == "y" || option == "Y") {
+                remove(arguments.input_file);
+                if(arguments.verbose)
+                    printf("Removed %s\n", arguments.input_file);
+            }
+            else if(arguments.verbose) {
+                printf("Will not remove %s\n");
+            }
+        }
+    }
+    else if(arguments.verbose)
+        printf("The file %s doesn't exist, could not remove.\n", arguments.input_file);
 }
