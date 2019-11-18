1f46147b0a97a790acd54810fe3b0bff01545fa9
Aidan Johnston <aidanjhnston@gmail.com>
2019-11-18 17:58:38 -0500

    Logic for move.h

--- move.h
+++ move.h
@@ -31,5 +31,113 @@
 #include "header.h"
 
 bool move(ARGS arguments) {
-    return true;
+    char* option = "";
+    bool flag = false;
+    if(doesFileExist(arguments.input_file)) {
+        if(arguments.force) {
+            FILE* input = fopen(arguments.input_file, "r");
+            if(input == NULL)
+                printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+            FILE* output =  fopen(arguments.output_file, "w");
+            if(output == NULL)
+                printf("Couldn't open %s\n", arguments.output_file), exit(1);
+
+            char ch = "";
+            while ((ch = fgetc(input)) != EOF)
+                fputc(ch, output);
+
+            remove(input);
+            fclose(input);
+            fclose(output);
+
+            if(arguments.verbose)
+                printf("Moved file\n");
+        }
+        else {
+            if(arguments.output) {
+                if(doesFileExist(arguments.output_file)) {
+                    printf("Are you sure you want to move %s?  %s already exist. (y/n)\n", arguments.input_file, arguments.output_file);
+                    scanf("%c", &option);
+
+                    while(!flag) {
+                        if(option == 121 || option == 110) {
+                            flag = true;
+                        }
+                        else {
+                            scanf("%c", &option);
+                            printf("Unknown answer, please try again: (y/n)\n");
+                            scanf("%c", &option);
+                        }
+                    }
+                    if(option == 121) {
+                        FILE* input = fopen(arguments.input_file, "r");
+                        if(input == NULL)
+                            printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+                        FILE* output = fopen(arguments.output_file, "w");
+                        if(output == NULL)
+                            printf("Couldn't open file\n"), exit(1);
+
+                        char ch = "";
+                        while((ch = fgetc(input)) != EOF)
+                            fputc(ch, output);
+
+                        remove(input);
+                        fclose(input);
+                        fclose(output);
+
+                        if(arguments.verbose)
+                            printf("Moved the file\n");
+                    }
+                    else if(arguments.verbose) {
+                        printf("Wont move the file\n");
+                    }
+                }
+                else {
+                    FILE* input = fopen(arguments.input_file, "r");
+                    if(input == NULL)
+                        printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+                    FILE* output = fopen(arguments.output_file, "w");
+                    if(output == NULL)
+                        printf("Couldn't open file\n"), exit(1);
+
+                    char ch = "";
+                    while((ch = fgetc(input)) != EOF)
+                        fputc(ch, output);
+
+                    remove(input);
+                    fclose(input);
+                    fclose(output);
+
+                    if(arguments.verbose)
+                        printf("Moved the file\n");
+                }
+            }
+            else {
+                FILE *input = fopen(arguments.input_file, "r");
+                if (input == NULL)
+                    printf("Couldn't open %s\n", arguments.input_file), exit(1);
+
+                FILE *output = fopen("default.txt", "w");
+                if (output == NULL)
+                    printf("Couldn't open file\n"), exit(1);
+
+                char ch = "";
+                while ((ch = fgetc(input)) != EOF)
+                    fputc(ch, output);
+
+                remove(input);
+                fclose(input);
+                fclose(output);
+
+                if (arguments.verbose)
+                    printf("Moved the file\n");
+            }
+        }
+    }
+    else if(arguments.verbose) {
+        printf("The file %s doesn't exist, could not move the file.\n", arguments.input_file);
+    }
 }
