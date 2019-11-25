a1886e36a9063aec9a265db28fdbaaac918de5df
Aidan Johnston <aidanjhnston@gmail.com>
2019-11-25 09:20:24 -0500

    Refactored file location

--- src/pofm.c
+++ src/pofm.c
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
 
 	//DEFAULT VALUES
 
-        arguments.input_file = "-";
+	arguments.input_file = "-";
 	arguments.verbose = false;
 	arguments.force = false;
 	arguments.output = false;
